3a5e9f970f86081b0b0ac68df3e02726fd9f1d7c
hdlguy <hdlguy@hdlguy.com>
2025-10-23 10:51:40 -0400

    comment

--- instrument_server/fpga_control/test_client.py
+++ instrument_server/fpga_control/test_client.py
@@ -5,10 +5,11 @@ import time
 print("Initializing instrument...")
 inst.fpga_open()
 
+
 for mode in [0, 1, 2, 3]:
 
     # Simulate waiting for the instrument to stabilize
-    time.sleep(0.2)
+    #time.sleep(0.2)
 
 
     status = inst.fpga_set_led(mode)
