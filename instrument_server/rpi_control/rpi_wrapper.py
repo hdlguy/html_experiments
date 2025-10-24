554922777232f24e5e1ea7f8cf35ef02ee955e71
hdlguy <hdlguy@hdlguy.com>
2025-10-24 13:27:03 -0400

    wrapping Rpi temperature read function

---
+++ instrument_server/rpi_control/rpi_wrapper.py
@@ -0,0 +1,13 @@
+
+import subprocess
+
+def read_cpu_temp() -> float:
+    """Read CPU temperature in Celsius."""
+    try:
+        with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
+            temp_str = f.read().strip()
+        return float(temp_str) / 1000.0
+    except Exception as e:
+        print(f"Error reading CPU temperature: {e}")
+        return float("nan")
+
---
+++ instrument_server/rpi_control/wrapper_test.py
@@ -0,0 +1,8 @@
+# This little module tests libinstrument_wrapper.py
+
+import rpi_wrapper as inst
+import time
+
+cpu_temperature = inst.read_cpu_temp()
+print(f'cpu_temperature = {cpu_temperature}')
+
