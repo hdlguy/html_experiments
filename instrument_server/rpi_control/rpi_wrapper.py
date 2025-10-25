2b4dbd5f0de1339a8cea7e91dcd9d8b59898288d
hdlguy <hdlguy@hdlguy.com>
2025-10-24 22:17:02 -0400

    python direct access to temperature

--- instrument_server/rpi_control/rpi_wrapper.py
+++ instrument_server/rpi_control/rpi_wrapper.py
@@ -4,10 +4,11 @@ import subprocess
 def read_cpu_temp() -> float:
     """Read CPU temperature in Celsius."""
     try:
-        with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
+        with open("/sys/class/thermal/thermal_zone3/temp", "r") as f:
             temp_str = f.read().strip()
         return float(temp_str) / 1000.0
     except Exception as e:
         print(f"Error reading CPU temperature: {e}")
         return float("nan")
 
+# /sys/class/hwmon/hwmon5/temp1_input
